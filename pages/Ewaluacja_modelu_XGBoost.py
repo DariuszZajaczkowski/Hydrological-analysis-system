498d03f8e5878a0d1d152a712b13e6cf44a8d0c2
DariuszZajaczkowski <dzajaczkowski1994@gmail.com>
2025-04-25 23:16:56 +0200

    Ewaluacja_modelu_XGBoost.py

--- pages/Ewaluacja_modelu_XGBoost.py
+++ pages/Ewaluacja_modelu_XGBoost.py
@@ -76,7 +76,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_
 model = xgb.XGBRegressor(objective="reg:squarederror", n_estimators=50,
                          learning_rate=0.1,
                          max_depth=4,
-                         min_child_weight=2,
+                         min_child_weight=1,
                          random_state=42)
 model.fit(X_train, y_train)
 
