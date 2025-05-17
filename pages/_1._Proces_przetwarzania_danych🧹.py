4d6fedb359efa5a343b340160aab0fba739d08be
DariuszZajaczkowski <dzajaczkowski1994@gmail.com>
2025-05-17 15:03:07 +0200

    Update _1._Proces_przetwarzania_danych🧹.py

--- pages/_1._Proces_przetwarzania_danych🧹.py
+++ pages/_1._Proces_przetwarzania_danych🧹.py
@@ -17,6 +17,6 @@ url = "https://github.com/DariuszZajaczkowski/Hydrological-analysis-system/raw/m
 st.image(url,  caption= "Ogólny proces przetwarzania danych w projekcie 'System Analiz Hydrologicznych")
 
 st.markdown("<h5 style='text-align: left;'>Pierwszy etap przetwarzania danych</h5>", unsafe_allow_html=True)
-url1 = "https://github.com/DariuszZajaczkowski/Hydrological-analysis-system/blob/main/ETL_iamges/ETL_1.png"
+url1 = "https://github.com/DariuszZajaczkowski/Hydrological-analysis-system/tree/main/ETL_iamges/ETL_1.png"
 with st.expander("Wyświetl fragment kodu:"):
     st.image(url1)
