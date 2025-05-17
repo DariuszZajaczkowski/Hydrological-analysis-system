601f693155b491087299479686784e05e12e4458
DariuszZajaczkowski <dzajaczkowski1994@gmail.com>
2025-05-17 14:45:46 +0200

    Update _6._Statystyka📊.py

--- pages/_6._Statystyka📊.py
+++ pages/_6._Statystyka📊.py
@@ -24,7 +24,7 @@ st.set_page_config(
 st.markdown("""
             <div style='text-align: left;'>
             <span style = "font-size: 20px; font-weight: bold;">
-            SEKCJA V - Analiza statystyczna
+            SEKCJA VI - Analiza statystyczna
             </span>
         </div>   
     """, unsafe_allow_html=True)
