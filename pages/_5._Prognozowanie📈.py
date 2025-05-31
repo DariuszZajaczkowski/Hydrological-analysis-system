7c0de6e78fb1a4b171efaad67485507c4cc308c4
DariuszZajaczkowski <dzajaczkowski1994@gmail.com>
2025-05-31 13:02:27 +0200

    Update _5._Prognozowanie📈.py

--- pages/_5._Prognozowanie📈.py
+++ pages/_5._Prognozowanie📈.py
@@ -44,9 +44,9 @@ y = model_data[target]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 #Tworzenie i trenowanie modelu
-model = xgb.XGBRegressor(objective="reg:squarederror", n_estimators=45,
-                         learning_rate=0.1,
-                         max_depth=3,
+model = xgb.XGBRegressor(objective="reg:squarederror", n_estimators=30,
+                         learning_rate=0.9,
+                         max_depth=2,
                          min_child_weight=1,
                          random_state=42)
 model.fit(X_train, y_train)
